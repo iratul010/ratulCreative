8377a944ffb964b8cdc7ceff70da31e9b8821feb
Ratul Islam <iratul010@gmail.com>
2024-07-28 11:17:21 +0600

    project-route-plan

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+ 
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route index element={<Navigate replace to="home" />} />
+        <Route path="home" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
+import Header from "../components/Header/Header";
+import Work from "../components/Work/Work";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Work />
+      <About />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
